8ab5847b62bae906e41fdbbe9f810c2e09d98166
takumi <ganndamu0706@gmail.com>
2024-09-26 17:57:12 +0900

    ✨ feat: ユーザーIDでユーザー情報を取得する関数を追加する
    🗑️ chore: テストページを削除する

--- nextjs/Data/user.ts
+++ nextjs/Data/user.ts
@@ -1,3 +1,19 @@
 //DBからUser情報を取る関数（getUserById)
 
+import { db } from "@/lib/db";
 
+export const getUserById = async (id: number) => {
+    try {
+        console.log(`Querying user with ID: ${id}`);
+        const user = await db.user.findUnique({
+            where: {
+                id,
+            },
+        });
+        console.log('Queried user:', user);
+        return user;
+    } catch (err) {
+        console.error('Error querying user:', err);
+        return null;
+    }
+}
